ee241b7118960596f4d0cb43ad6fa1b46b004bf2
choijaeyeong <sylove532@gmail.com>
2020-08-10 11:15:54 +0900

    first commit

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+public static void main(String[] args){
+	System.out.println("Hello , welcome to the java world!");
+	System.out.println("Nice meet you");
+	int myage = 27;
+	System.out.println("My age is " +myage);
+	}
+}
