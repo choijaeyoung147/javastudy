010ce97bcbf71d9b45f62b6bffb43df74fb75935
choijaeyeong <sylove532@gmail.com>
2020-08-10 12:43:03 +0900

    six

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test{
+	public static void main (String[] args){
+		int value1 = 10;
+		int value2 = 20;
+		int result = value1+value2;
+		System.out.println(value1+"+"+value2+"="+result);
+	}
+}
