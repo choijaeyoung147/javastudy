1799b71549ff24670643d2db96631e66abf4627b
choijaeyeong <sylove532@gmail.com>
2020-08-10 12:34:27 +0900

    five

--- VariableExample.java
+++ VariableExample.java
@@ -1,8 +1,8 @@
-Public class VariableExample{
+public class VariableExample{
 	public static void main (String[] args){
 		int value1 = 10;
 		int value2 = 20;
 		int result = value1+value2;
-		System.out.println(value1+"+"+y+"="result);
+		System.out.println(value1+"+"+value2+"="+result);
 	}
 }
